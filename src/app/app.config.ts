e156fa9ead7020dd337644b133b2bcb19933687a
mohamed mazrou <mohamedmazrou994@gmail.com>
2025-10-13 13:46:49 +0300

    update angular fire from v16 to v17

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,37 +1,32 @@
-import { ApplicationConfig, importProvidersFrom } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import { ApplicationConfig } from '@angular/core';
+import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
-
-import { HttpClient, HttpClientModule } from '@angular/common/http';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
-import { getFirestore, provideFirestore } from '@angular/fire/firestore';
-import {  withInMemoryScrolling } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 import { provideEcharts } from 'ngx-echarts';
 
+// firebase
+import { initializeApp } from 'firebase/app';
+import { provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getStorage, provideStorage } from '@angular/fire/storage';
 
-import { provideAuth, getAuth } from '@angular/fire/auth';
 import { environment } from '../app/environment/environment ';
-
-
+import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideRouter(routes),
+    provideRouter(routes, withInMemoryScrolling({ scrollPositionRestoration: 'enabled' })),
     provideClientHydration(),
-    importProvidersFrom(HttpClientModule),
-
-    importProvidersFrom(
-      provideFirebaseApp(() => initializeApp(environment.firebase)),
-      provideAuth(() => getAuth()),
-      provideFirestore(() => getFirestore())
-    ),
-
+    provideHttpClient(),
     provideAnimations(),
     provideEcharts(),
 
-    
+
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFirestore(() => getFirestore()),
+    provideAuth(() => getAuth()),
+    provideStorage(() => getStorage()),
   ],
 };
